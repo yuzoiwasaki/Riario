22c46aea77a276b70dda35b5d5408f4075e3abb7
yuzoiwasaki <a0556017@sophia.jp>
2015-05-06 11:10:43 +0900

    update user interface

--- db/migrate/20150506013606_add_owner_id_to_nikkis.rb
+++
@@ -1,5 +0,0 @@
-class AddOwnerIdToNikkis < ActiveRecord::Migration
-  def change
-    add_column :nikkis, :owner_id, :string
-  end
-end
--- db/migrate/20150506014353_remove_owner_id_to_nikkis.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOwnerIdToNikkis < ActiveRecord::Migration
-  def change
-    remove_column :nikkis, :owner_id, :string
-  end
-end
