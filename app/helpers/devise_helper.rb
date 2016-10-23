28b2189e21fe076d1112bd794b873662927cdf58
yuzoiwasaki <a0556017@sophia.jp>
2016-10-24 00:02:42 +0900

    join error messages into one box

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,18 +2,17 @@ module DeviseHelper
   def devise_error_messages!
     return "" if resource.errors.empty?
 
-    html = ""
-    messages = resource.errors.full_messages.each do |errmsg|
-      html += <<-EOF
-      <div class="alert alert-danger alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert">
-          <span aria-hidden="true">&times;</span>
-          <span class="sr-only">close</span>
-        </button>
-        #{errmsg}
+    html = errmsg = ""
+    resource.errors.full_messages.each do |msg|
+      errmsg += "<li>#{msg}</li>"
+    end
+    html += <<-EOF
+      <div class="alert alert-danger">
+        <ul>
+          #{errmsg}
+        </ul>
       </div>
       EOF
-    end
     html.html_safe
   end
 end
